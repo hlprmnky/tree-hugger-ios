4a1a68336f583ffc1a646ef506445736ade18de9
Chris Johnson Bidler <chris@hlprmnky.com>
2015-05-15 22:09:54 -0400

    Initial Xcode project creation

---
+++ TreeHuggerMichiana/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  TreeHuggerMichiana
+//
+//  Created by Chris Johnson Bidler on 5/15/15.
+//  Copyright (c) 2015 Chris Johnson Bidler. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
